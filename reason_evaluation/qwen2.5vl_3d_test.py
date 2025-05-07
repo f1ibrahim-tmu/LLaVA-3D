bed7c8ab7fa412019a679bca2633fc488043ce16
Leihan Chen <leihan.chen@torontomu.ca>
2025-05-07 15:49:41 +0000

    Run VL2.5 with SQA3D low performance scene

--- reason_evaluation/qwen2.5vl_3d_test.py
+++ reason_evaluation/qwen2.5vl_3d_test.py
@@ -2,7 +2,8 @@ from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
 from qwen_vl_utils import process_vision_info
 import os
 import torch
-
+import json
+import gc
 
 def load_images(folder_path: str):
     """
@@ -24,10 +25,69 @@ def load_images(folder_path: str):
     return regular_images, depth_images
 
 
+def load_questions_json(file_path: str):
+    """
+    Load questions from a JSON file.
+    Args:
+        file_path (str): Path to the JSON file.
+    Returns:
+        list: List of questions.
+    """
+    with open(file_path, "r") as f:
+        questions = json.load(f)
+    return questions
+
+
+def add_answers_to_questions(questions: list, answer_str: str):
+    """
+    Add answers to questions.
+    Args:
+        questions (list): List of questions.
+        answer_str (str): Answer string to add.
+    Returns:
+        list: List of questions with answers.
+    """
+
+    # load answer from json file
+    with open(answer_str, "r") as f:
+        answers = json.load(f)
+    for i, question in enumerate(questions):
+        question["answer"] = answers[i]["text"]
+        question["type"] = answers[i]["type"]
+    return questions
+
+
+def find_image_paths(questions: list, folder_path: str, sample_rate: int = 1):
+    """
+    Find image paths in questions.
+    Args:
+        questions (list): List of questions.
+        folder_path (str): Path to the folder containing images.
+        sample_rate (int): Sample rate for images.
+    Returns:
+        list: questions List with image paths.
+    """
+    image_paths = []
+    for question in questions:
+        scene_name = question["video"]
+        scene_folder_path = os.path.join(folder_path, scene_name, scene_name+"_sens", "color")
+        # add all jpg files in the folder to the image_paths list
+        count = 0
+        for filename in os.listdir(scene_folder_path):
+            if filename.endswith(".jpg"):
+                count += 1
+                if count % sample_rate != 0:
+                    continue
+                img_path = os.path.join(scene_folder_path, filename)
+                image_paths.append(img_path)
+        question["scene_images_path"] = image_paths
+    return questions
+
+
 # You can set the maximum tokens for a video through the environment variable VIDEO_MAX_PIXELS
 # based on the maximum tokens that the model can accept.
 # export VIDEO_MAX_PIXELS = 32000 * 28 * 28 * 0.9
-def main(image_paths: str, text_prompt: str, model_path: str):
+def qwen_video_test(image_paths: list, text_prompt: str, model_path: str, device: str = "cuda:2"):
     # You can directly insert a local file path, a URL, or a base64-encoded image into the position where you want in the text.
     messages = [
         # Image
@@ -95,12 +155,22 @@ def main(image_paths: str, text_prompt: str, model_path: str):
                     {
                         "type": "video",
                         "video": image_paths,
-                        # "resized_height": 280,
-                        # "resized_width": 280,
+                        "resized_height": 280,
+                        "resized_width": 280,
+                        # "fps": 30.0,
                     },
                     {"type": "text", "text": text_prompt},
                 ],
-            }
+            },
+            {
+                "role": "assistant",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "You are a help assistant to answer the question with your separate reasoning trace.",
+                    }
+                ],
+            },
         ],
         ## Model dynamically adjusts video nframes, video height and width. specify args if required.
         # [
@@ -123,7 +193,7 @@ def main(image_paths: str, text_prompt: str, model_path: str):
     model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
         model_path,
         torch_dtype=torch.bfloat16,
-        device_map="cuda:2",
+        device_map=device,
         attn_implementation="flash_attention_2",
     )
 
@@ -134,33 +204,82 @@ def main(image_paths: str, text_prompt: str, model_path: str):
     images, videos, video_kwargs = process_vision_info(
         messages, return_video_kwargs=True
     )
-    inputs = processor(
-        text=text,
-        images=images,
-        videos=videos,
-        padding=True,
-        return_tensors="pt",
-        **video_kwargs,
-    ).to(model.device)
+    with torch.no_grad():
+        inputs = processor(
+            text=text,
+            images=images,
+            videos=videos,
+            padding=True,
+            return_tensors="pt",
+            **video_kwargs,
+        ).to(model.device)
 
-    # Generate the output
-    generated_ids = model.generate(**inputs, max_new_tokens=128)
-    generated_ids_trimmed = [
-        out_ids[len(in_ids) :]
-        for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
-    ]
-    output_text = processor.batch_decode(
-        generated_ids_trimmed,
-        skip_special_tokens=True,
-        clean_up_tokenization_spaces=False,
-    )
-    print(output_text)
+        # Generate the output
+        generated_ids = model.generate(**inputs, max_new_tokens=128)
+        generated_ids_trimmed = [
+            out_ids[len(in_ids) :]
+            for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+        ]
+        output_text = processor.batch_decode(
+            generated_ids_trimmed,
+            skip_special_tokens=True,
+            clean_up_tokenization_spaces=False,
+        )
+    gc.collect()
+    torch.cuda.empty_cache()
+    return output_text
+
+
+def save_output(output_text: str, question: dict, output_file_path:str):
+    # Parse the output text to extract the reasoning and answer
+    # This is a placeholder implementation. You may need to adjust it based on the actual output format.
+    try:
+        output_json = json.loads(output_text)
+        reasoning = output_json.get("reason", "")
+        answer = output_json.get("answer", "")
+        # append this new json enry to the output file
+        with open(output_file_path, "a") as f:
+            json.dump(
+                {
+                    "reason": reasoning,
+                    "answer": answer,
+                    "question_id": question["question_id"],
+                    "scene_name": question["video"],
+                    "question": question["text"],
+                },
+                f,
+            )
+            f.write("\n")
+    except json.JSONDecodeError:
+        print(f"Failed to parse output: {output_text}")
+
+
+def main(question_file_path:str, answer_file_path:str, image_folder_path:str, export_json_path:str, model_path:str):
+    # Load questions from a JSON file
+    questions = load_questions_json(question_file_path)
+    # Load answers from a JSON file
+    questions = add_answers_to_questions(questions, answer_file_path)
+    # Find image paths in questions
+    questions = find_image_paths(questions, image_folder_path, 500)
+
+    for question in questions:
+        # print(question)
+        # Get the image paths for the first question
+        image_paths = question["scene_images_path"]
+        # Get the text prompt for the first question
+        question_text = question["text"]
+        # Get text prompt from the question
+        text_prompt = question_text + " Please reason step by step, and give your reason and answer in the json format:{\"reason\": \"\", \"answer\": \"\"}."
+        # Run the Qwen video test
+        output_text = qwen_video_test(image_paths, text_prompt, model_path)
+        save_output(output_text, question, export_json_path)
 
 
 if __name__ == "__main__":
     # Load images from a folder
-    folder_path = "/root/research_projects/LLaVA-3D/demo/scannet/posed_images/scene0356_00"
+    question_file_path = "/data/SceneUnderstanding/7792397/ScanQA_format/SQA_em1-below-35_formatted_LLaVa3d.json"
+    answer_file_path = "/data/SceneUnderstanding/7792397/ScanQA_format/SQA_em1-below-35_formatted_LLaVa3d_answers.json"
     model_path = "Qwen/Qwen2.5-VL-7B-Instruct"
-    regular_images, depth_images = load_images(folder_path)
-    text_prompt = "Tell me the only object that I could see from the other room and describe the object."
-    main(regular_images, text_prompt, model_path)
+    image_folder_path = "/data/SceneUnderstanding/ScanNet/scans"
+    export_path = "/root/research_projects/LLaVA-3D/reason_evaluation/qwen2.5vl_3d_test_results.json"
+    main(question_file_path, answer_file_path, image_folder_path, export_path, model_path)
